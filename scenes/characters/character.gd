0ed36d89e978a3cce6ee7bccb0fe37817a5524b3
Tim Lines <timlines89@gmail.com>
2026-06-06 17:57:58 -0500

    Player Animations Debugged

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -13,6 +13,13 @@ var movement_input: Vector2
 @onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 @onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
+@onready var move_state_machine = $AnimationTree.get("parameters/MoveStateMachine/playback") as AnimationNodeStateMachinePlayback
+
 
 func apply_gravity(gravity, delta):
 	velocity.y -= gravity * delta
+
+
+func set_move_state(state_name: String):
+	#print("Travel to: ", state_name)
+	move_state_machine.travel(state_name)
--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -21,18 +21,23 @@ func move_logic(delta):
 		velocity.z = vel_2d.y
 		var target_angle = -movement_input.angle() + PI / 2
 		skin.rotation.y = rotate_toward(skin.rotation.y, target_angle, delta *  6)
+		set_move_state('Running_A')
 		
 	else:
 		vel_2d = vel_2d.move_toward(Vector2.ZERO, base_speed * delta * deceleration)
 		velocity.x = vel_2d.x
 		velocity.z = vel_2d.y
+		#print(movement_input)
+		set_move_state('Idle')
+	
 		
 
 func jump_logic(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed('jump'):
 			velocity.y = -jump_velocity
-			
+	else:
+		set_move_state('Jump_Idle')		
 	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
 	
 	apply_gravity(gravity, delta)
